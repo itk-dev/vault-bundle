0af2323a15918a2b0cfbe9c11af09402a021b0e2
Jesper Kristensen <cableman@linuxdev.dk>
2024-10-14 16:47:06 +0200

    Fixed code style

--- src/ItkDevVaultBundle.php
+++ src/ItkDevVaultBundle.php
@@ -2,13 +2,10 @@
 
 namespace ItkDev\VaultBundle;
 
-use ItkDev\VaultBundle\Processor\VaultEnvResolver;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
-use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 
 class ItkDevVaultBundle extends AbstractBundle
 {
