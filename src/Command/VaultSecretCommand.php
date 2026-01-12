1f08e143fae7bf6d827a3eca8b4ea60142a31365
Mikkel Ricky <rimi@aarhus.dk>
2026-01-12 14:56:03 +0100

    Renamed option

--- src/Command/VaultSecretCommand.php
+++ src/Command/VaultSecretCommand.php
@@ -34,7 +34,7 @@ class VaultSecretCommand extends Command
         $this
             ->addOption('path', null, InputOption::VALUE_REQUIRED, 'Vault secret engine path)')
             ->addOption('secret', null, InputOption::VALUE_REQUIRED, 'Name of the secret to fetch')
-            ->addOption('keys', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'List of secret keys to fetch')
+            ->addOption('key', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'List of secret keys to fetch')
             ->addOption('version-id', null, InputOption::VALUE_REQUIRED, 'Version of the secret to fetch')
             ->addOption('useCache', null, InputOption::VALUE_NONE, 'Cache the token and secrets fetched')
             ->addOption('expire', null, InputOption::VALUE_REQUIRED, 'For how long the secrets should be cached (in seconds). The token will be cached based on its expiration time.')
@@ -61,7 +61,7 @@ class VaultSecretCommand extends Command
         if (null === $secret) {
             throw new InvalidArgumentException('The secret option is required.');
         }
-        $keys = $input->getOption('keys');
+        $keys = $input->getOption('key');
         if (empty($keys)) {
             throw new InvalidArgumentException('At least one key must be specified.');
         }
